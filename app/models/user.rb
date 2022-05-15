1dab55fffd684b3fed98648cf336476cfeaf2031
Nigel Yeo <nigelyyz@yahoo.com.sg>
2022-05-15 13:59:10 +0800

    Added User database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    # Validations
+    validates :username, presence: true, length: { minimum: 7, maximum: 25}, uniqueness: true, on: :create # Name must be 10-25 characters long and must be unique
 end
