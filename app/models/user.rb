0a0689ce13b481c01e9f183c70f07de259deb16a
Nigel Yeo <nigelyyz@yahoo.com.sg>
2022-05-15 15:05:10 +0800

    Added posts and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
     # Validations
-    validates :username, presence: true, length: { minimum: 7, maximum: 25}, uniqueness: true, on: :create # Name must be 10-25 characters long and must be unique
+  validates :username, presence: true, length: { minimum: 7, maximum: 25}, uniqueness: true # Name must be 10-25 characters long and must be unique
+  validates :email, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_15_050317) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_15_063336) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
